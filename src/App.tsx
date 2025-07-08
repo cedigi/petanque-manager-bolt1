37d910ae9fa9873d429d06823723ad45e7d84d91
cedigi <cedigi07@hotmail.fr>
2025-07-08 07:25:23 +0200

    feat(animation): add four more floating balls

--- src/App.tsx
+++ src/App.tsx
@@ -144,6 +144,11 @@ function App() {
         <div className="petanque-ball"></div>
         <div className="petanque-ball"></div>
         <div className="petanque-ball"></div>
+        <div className="petanque-ball"></div>
+        <div className="petanque-ball"></div>
+        <div className="petanque-ball"></div>
+        <div className="petanque-ball"></div>
+        <div className="petanque-ball"></div>
       </div>
 
       <Header animationsPaused={animationsPaused} onToggleAnimations={toggleAnimations} />
