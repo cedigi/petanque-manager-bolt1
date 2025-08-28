adae9239bab4850f7523f604c698c7306b814aa6
cedigi <cedigi07@hotmail.fr>
2025-08-28 18:27:01 +0200

    Use public logo asset

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,3 @@
-import React from 'react';
-import logoUrl from '../../logo1.png';
-
-interface LogoProps {
-  className?: string;
-}
-
-export function Logo({ className = "w-12 h-12" }: LogoProps) {
-  return (
-    <img src={logoUrl} alt="Logo" className={className} />
-  );
+export function Logo({ className = "w-12 h-12" }) {
+  return <img src="/logo1.png" alt="Logo" className={className} />;
 }
